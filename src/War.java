9a4cb417c5160ebf9d8de41eab94a3d369e26b5e
Collin <bauercl@g.cofc.edu>
2018-12-01 13:42:09 -0500

    Added War

---
+++ src/War.java
@@ -0,0 +1,23 @@
+/*
+ * War.java
+ *
+ * Plays a game of War.
+ *
+ */
+
+public class War{
+
+    public static void main(String[] args){
+        System.out.println("Let's play war!");
+
+        Deck player1 = new Deck(false);
+        player1.shuffle();
+        Deck player2 = warDeck.split();
+
+        // debug prints
+        System.out.println("Decks created successfully. Checking their contents below...\n");
+        System.out.printf("Player 1's deck...%s\n\n",player1.toString());
+        System.out.printf("Player 1's deck...%s\n\n",player2.toString());
+
+    }
+}
