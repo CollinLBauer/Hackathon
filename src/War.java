36921b52c45c7ec3b03b6017ee363202503d7704
Collin <bauercl@g.cofc.edu>
2018-12-01 18:30:06 -0500

    It do work that way sometime.

--- src/War.java
+++ src/War.java
@@ -5,16 +5,42 @@
  *
  */
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class War{
 
-    private static int compareCards(Card card1, Card card2){
-        return card1.getNumber() - card2.getNumber();
+    private static void compareCards(Deck deck1, Deck deck2,
+                                                Deck discard1, Deck discard2,
+                                                ArrayList<Card> pile){
+        Card card1 = deck1.getTopCard();
+        Card card2 = deck2.getTopCard();
+        System.out.printf("Comparing %s to %s\n",card1.toString(),card2.toString());
+
+        pile.add(card1);
+        pile.add(card2);
+
+        if (card1.getNumber() - card2.getNumber() > 0){
+            System.out.println("Player 1 wins round.");
+            for (int i = 0; i < pile.size(); i++){
+                discard1.addCard(pile.get(i));
+            }
+        }
+        else if (card1.getNumber() - card2.getNumber() < 0){
+            System.out.println("Player 2 wins round.");
+            for (int i = 0; i < pile.size(); i++){
+                discard2.addCard(pile.get(i));
+            }
+        }
+        else{
+            System.out.println("Cards tied. Calling recusively."); //debug
+            compareCards(deck1, deck2, discard1, discard2, pile);
+
+        }
     }
 
     private static void playWar(Scanner scnr){
-        System.out.println("Let's play war!");
+        System.out.println("Let's play war!\n");
 
         // initialize decks
         Deck player1 = new Deck(false);
@@ -24,14 +50,21 @@ public class War{
         Deck discard1 = new Deck();
         Deck discard2 = new Deck();
 
-        Card card1;
-        Card card2;
-        for (int i = 0; i < player1.getSize(); i++){
-            card1 = player1.getTopCard();
-            card2 = player2.getTopCard();
-            if (compareCards(card1, card2)
+        ArrayList<Card> dummy = new ArrayList<Card>();
 
+        for (int i = 0; i < player1.getSize(); i++){
+            compareCards(player1, player2, discard1, discard2, dummy);
         }
+        if (discard1.getSize() > discard2.getSize()){
+            System.out.println("\nPlayer 1 wins!");
+        }
+        else if (discard1.getSize() < discard2.getSize()){
+            System.out.println("\nPlayer 2 wins!");
+        }
+        else{
+            System.out.println("Game tied.");
+        }
+
 
     }
 
